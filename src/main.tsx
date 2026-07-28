910483e4ed00ae0c0ddb40e4bba5fc24151ed868
bankheleatharv12 <bankheleatharv12@gmail.com>
2026-07-28 21:00:12 +0530

    title updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./app/App.tsx";
 import "./styles/index.css";
 
 const faviconLink = document.createElement("link");
 faviconLink.rel = "icon";
 faviconLink.type = "image/png";
 faviconLink.href = "/ChatGPT%20Image%20Jul%2028%2C%202026%2C%2008_54_04%20PM.png";
+document.head.appendChild(faviconLink);
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
   
